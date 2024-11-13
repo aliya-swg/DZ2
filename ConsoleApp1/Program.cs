26ef6eeaa332c283978dc91b460407f0a7e3d618
User <User@WIN-2R8ETCIAQT5>
2024-11-14 00:30:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp1.Program;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskL31();
+            TaskL32();
+            Task33();
+
+
+        }
+        //Тумаков.Лабораторная 3 главы
+        //Упражнение 3.1.  Создать перечислимый тип данных отображающий виды банковского счета(текущий и сберегательный). Создать переменную типа перечисления, присвоить ей значение и вывести это значение на печать.
+        public enum AccountType
+        {
+            actual,
+            saving
+        }
+        static void TaskL31()
+        {
+            AccountType accountType = AccountType.saving;
+            Console.WriteLine($"Тип банковского счета: {accountType}");
+        }
+        //Упражнение 3.2. Создать структуру данных, которая хранит информацию о банковском счете – его номер, тип и баланс.Создать переменную такого типа, заполнить структуру значениями и напечатать результат.
+        struct AccountInfo
+        {
+            /// <summary>
+            /// Номер счета
+            /// </summary>
+            public int Number;
+            /// <summary>
+            /// Тип счета
+            /// </summary>
+            public string Type;
+            /// <summary>
+            /// Баланс
+            /// </summary>
+            public decimal Balance;
+        }
+        static void TaskL32()
+        {
+            AccountInfo account;
+
+            // Заполнение структуры значениями
+            account.Number = 1234567890;
+            account.Type = "Текущий";
+            account.Balance = 1000;
+
+            // Вывод информации о банковском счете
+            Console.WriteLine($"Номер счета: {account.Number}");
+            Console.WriteLine($"Тип счета: {account.Type}");
+            Console.WriteLine($"Баланс: {account.Balance}");
+        }
+        // Упражнение 3.3. Создать перечислимый тип ВУЗ{КГУ, КАИ, КХТИ}. Создать структуру работник с двумя полями: имя, ВУЗ.Заполнить структуру данными распечатать.
+        public enum University
+        {
+            КГУ,
+            КАИ,
+            КХТИ
+        }
+        public struct Worker
+        {
+            /// <summary>
+            /// Имя 
+            /// </summary>
+            public string Name;
+            /// <summary>
+            /// ВУЗ
+            /// </summary>
+            public University University;
+        }
+        static void Task33()
+        {
+            Worker worker;
+
+            worker.Name = "Екатерина Владимировна";
+            worker.University = University.КГУ;
+
+            Console.WriteLine($"Имя: {worker.Name}");
+            Console.WriteLine($"ВУЗ: {worker.University}");
+        }
+    }
+}
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+            
+      
+              
+        }
+        //1. Выведите на экран информацию о каждом типе данных в виде:  Тип данных – максимальное значение – минимальное значение
+        static void Task1()
+        {
+            // Целочисленные типы данных
+            Console.WriteLine("Тип данных - Максимальное значение - Минимальное значение");
+            Console.WriteLine($"sbyte - {sbyte.MaxValue} - {sbyte.MinValue}");
+            Console.WriteLine($"byte - {byte.MaxValue} - {byte.MinValue}");
+            Console.WriteLine($"short - {short.MaxValue} - {short.MinValue}");
+            Console.WriteLine($"ushort - {ushort.MaxValue} - {ushort.MinValue}");
+            Console.WriteLine($"int - {int.MaxValue} - {int.MinValue}");
+            Console.WriteLine($"uint - {uint.MaxValue} - {uint.MinValue}");
+            Console.WriteLine($"long - {long.MaxValue} - {long.MinValue}");
+            Console.WriteLine($"ulong - {ulong.MaxValue} - {ulong.MinValue}");
+
+            // Числа с плавающей точкой
+            Console.WriteLine($"float - {float.MaxValue} - {float.MinValue}");
+            Console.WriteLine($"double - {double.MaxValue} - {double.MinValue}");
+            Console.WriteLine($"decimal - {decimal.MaxValue} - {decimal.MinValue}");
+
+            // Символьный тип данных
+            Console.WriteLine($"char - {char.MaxValue} - {char.MinValue}");
+
+            // Логический тип данных
+            Console.WriteLine($"bool - {bool.TrueString} - {bool.FalseString}");
+        }
+        //2. Напишите программу, в которой принимаются данные пользователя в виде имени,рода, возраста и PIN-кода.Далее сохраните все значение в соответствующей еременной, а затем распечатайте всю информацию в правильном формате
+        struct Data
+        {
+            /// <summary>
+            /// имя
+            /// </summary>
+            public string Name;
+            /// <summary>
+            /// город
+            /// </summary>
+            public string City;
+            /// <summary>
+            /// возраст
+            /// </summary>
+            public int Age;
+            /// <summary>
+            /// пин-код
+            /// </summary>
+            public int PIN;
+        }
+        static void Task2()
+        {
+            Data personalData;
+            Console.Write("введите свое имя: ");
+            personalData.Name = Console.ReadLine();
+            Console.Write("введите свой город: ");
+            personalData.City = Console.ReadLine();
+            Console.Write("введите свой возраст: ");
+            personalData.Age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("введите свой пин-код: ");
+            personalData.PIN = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("имя: " + personalData.Name);
+            Console.WriteLine("город: " + personalData.City);
+            Console.WriteLine("возраст: " + personalData.Age);
+            Console.WriteLine("пин-код: " + personalData.PIN);
+        }
+        //3. Преобразуйте входную строку: строчные буквы замените на заглавные, заглавные – на строчные
+        static void Task3()
+        {
+            // Ввод входной строки
+            Console.WriteLine("Введите строку:");
+            string inputString = Console.ReadLine();
+
+            // Преобразование регистра букв
+            string outputString = "";
+            foreach (char character in inputString)
+            {
+                if (char.IsUpper(character))
+                {
+                    outputString += char.ToLower(character);
+                }
+                else if (char.IsLower(character))
+                {
+                    outputString += char.ToUpper(character);
+                }
+                else
+                {
+                    outputString += character; // Сохранение небуквенных символов
+                }
+            }
+
+            // Вывод преобразованной строки
+            Console.WriteLine($"Преобразованная строка: {outputString}");
+        }
+        //4. Введите строку, введите подстроку. Необходимо найти количество вхождений и вывести на экран
+        static void Task4()
+        {
+            // Ввод строки и подстроки
+            Console.WriteLine("Введите строку:");
+            string строка = Console.ReadLine();
+
+            Console.WriteLine("Введите подстроку:");
+            string подстрока = Console.ReadLine();
+
+            // Подсчет количества вхождений
+            int count = 0;
+            int index = 0;
+            while ((index = строка.IndexOf(подстрока, index)) != -1)
+            {
+                count++;
+                index += подстрока.Length;
+            }
+
+            Console.WriteLine($"Количество вхождений подстроки: {count}");
+        }
+    }
+}
